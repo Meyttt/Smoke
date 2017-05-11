df2ee4d36f382f06e2e544db96191423d2e8e4d3
a.chebotareva <sabermeyt@gmail.com>
2017-05-11 15:08:00 +0300

    Первый смоук готов

--- src/test/java/Smoke.java
+++ src/test/java/Smoke.java
@@ -1,5 +1,6 @@
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
@@ -53,16 +54,63 @@ public class Smoke {
     void PageAndLinks(){
         //наличие надписи
         chromeDriver.get(config.get("url"));
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/div[1]/div/div/div/div[1]/h3")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/div[1]/div/div/div/div[1]/h3")));
         Assert.assertTrue(Pattern.matches("Данный ресурс предназначен[\\w\\s]+для операторов связи",chromeDriver.findElementByXPath("//*[@id=\"wrapper\"]/ng-component/div[1]/div[1]/div[2]/div/div/div/div/div/div/p").getText()));
         //Проверка ссылки "О связи"
         chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div[3]/div/div/p/button[1]")).click();
-//        chromeDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"on_communication\"]/div/div[1]/h3")));
         Assert.assertTrue(Pattern.matches("Обязанности операторов[\\w\\s]+связи",chromeDriver.findElementByXPath("//*[@id=\"on_communication\"]/div/div[1]/h3").getText()));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[title='Close']")));
         chromeDriver.findElement(By.cssSelector("a[title='Close']")).click();
-
-
+        chromeDriver.manage().timeouts().implicitlyWait(100,TimeUnit.MILLISECONDS);
+        //Проверка "КоАП"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div[3]/div/div/p/button[2]")));
+        chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/div[2]/div/div[3]/div/div/p/button[2]")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"provide_info\"]/div/div[1]/h3")));
+        Assert.assertTrue(Pattern.matches("Ответственность за[\\w\\s]+непредоставление сведений",chromeDriver.findElementByXPath("//*[@id=\"provide_info\"]/div/div[1]/h3").getText()));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("a[title='Close']")));
+        chromeDriver.findElement(By.cssSelector("a[title='Close']")).click();
+        chromeDriver.manage().timeouts().implicitlyWait(100,TimeUnit.MILLISECONDS);
+        //Переходим на страницу "Статистика"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[ng-reflect-router-link='statistics']")));
+        chromeDriver.findElement(By.cssSelector("a[ng-reflect-router-link='statistics']")).click();
+        //cкачиваем файл
+        WebElement webElement =chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[4]/ul[2]/li[1]/div/a"));
+        Assert.assertEquals(webElement.findElement(By.tagName("h4")).getText(),"Абоненты ШПД (I квартал 2016 года)");
+        webElement.click(); //TODO: добавить проверку, скачплось ли
+        //Переходим на страницу "События и документы"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[ng-reflect-router-link='events']")));
+        chromeDriver.findElement(By.cssSelector("a[ng-reflect-router-link='events']")).click();
+        //На странице присутствуют строки "События", "Письмо Минкомсвязи России о порядке предоставления статистической отчетности"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.partialLinkText("События")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[2]/ul/li[1]/div/div[2]/p")));
+        Assert.assertTrue(chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[2]/ul/li[1]/div/div[2]/p")).getText().contains("Письмо Минкомсвязи России о порядке предоставления статистической отчетности"));
+        //Переходим на вкладку "Документы"
+        chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[1]/div/div/div/ul/li[2]")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[3]/ul/li[1]/div/a")));
+        //На странице присутствуют строки "Документы", "Письмо Минкомсвязи России о порядке предоставления статистической отчетности"
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[1]/div/div/div/ul/li[2]")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[3]/ul/li[1]/div/a/h4")));
+        Assert.assertTrue(chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[3]/ul/li[1]/div/a/h4")).getText().contains("Письмо Минкомсвязи России от 29.12.2016 №НН-П14-062-28741 «О порядке"));
+        Assert.assertTrue(chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[3]/ul/li[1]/div/a/h4")).getText().contains("представления статистической отчетности»"));
+        //Скачивается файл %D0%9D%D0%9D-%D0%9F14-062-28741.rar по ссылке Письмо Минкомсвязи России от 29.12.2016 №НН-П14-062-28741 «О порядке представления статистической отчетности»
+        chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div[3]/ul/li[1]/div/a")).click();
+        //Переходим на страницу "Вопросы и ответы"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[ng-reflect-router-link='faq']")));
+        chromeDriver.findElement(By.cssSelector("a[ng-reflect-router-link='faq']")).click();
+        //На странице присутствуют строки "Часто задаваемые вопросы", "Где можно ознакомиться с порядком предоставления статистической отчетности в сфере связи?"
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("blc_frequently_questions_ttl")));
+        Assert.assertTrue(chromeDriver.findElement(By.className("blc_frequently_questions_ttl")).findElement(By.tagName("h3")).getText().equals("Часто задаваемые вопросы"));
+        Assert.assertTrue(chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/ul/li[1]/div[1]/h4")).getText().contains("Где можно ознакомиться с порядком предоставления статистической отчетности в сфере связи?"));
+        //Переходим на страницу "Контакты"
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[ng-reflect-router-link='contacts']")));
+        chromeDriver.findElement(By.cssSelector("a[ng-reflect-router-link='contacts']")).click();
+        //"На странице присутствуют строки ""Адрес 125375, г. Москва, ул. Тверская, д. 7"""
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div/div/ul/li[1]/div/p")));
+        Assert.assertTrue(chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div/div/ul/li[1]/div/p")).getText().contains("Адрес"));
+        Assert.assertTrue(chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div/div/ul/li[1]/div/span[1]")).getText().contains("125375, г. Москва,"));
+        Assert.assertTrue(chromeDriver.findElement(By.xpath("//*[@id=\"wrapper\"]/ng-component/div[2]/div/div/ul/li[1]/div/span[2]")).getText().contains("ул. Тверская, д. 7"));
     }
     @AfterClass
     public void closeDriver(){
@@ -71,4 +119,5 @@ public class Smoke {
             chromeDriver.quit();
         }
     }
+
 }
